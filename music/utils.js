04eae760f467a1c514903d2bd56c4fa7fd2e2916
smsguy927 <smsguy927@gmail.com>
2023-03-09 17:28:43 -0600

    Added Http Response map

---
+++ music/utils.js
@@ -0,0 +1,27 @@
+const http = {
+    good: {
+        OK: 200,
+        created: 201,
+        noContent: 204
+    },
+    moved: {
+        permanently: 301,
+        temporarily: 302
+    },
+    bad: {
+        user: {
+            misc: 400,
+            unauthorized: 401,
+            forbidden: 403,
+            notFound: 404,
+            methodNotAcceptable: 405,
+            timeOut: 408
+        },
+        server: {
+            misc: 500,
+            notImplemented: 501,
+            badGateway: 502,
+            unavailable: 503
+        }
+    }
+}
